3bca435d8e6abbbdde04ecdbe2b069cb5c845e50
rog-works <rog_works@yahoo.co.jp>
2018-06-16 17:42:35 +0900

    feat: update mail settings

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ ! -e /etc/postfix/sasl_passwd.db ]; then
+	sed -i -e "s/some_address/${GMAIL_ADDRESS}/g" /etc/postfix/sasl_passwd
+	sed -i -e "s/some_password/${GMAIL_PASSWORD}/g" /etc/postfix/sasl_passwd
+	chmod 600 /etc/postfix/sasl_passwd
+	postmap /etc/postfix/sasl_passwd
+fi
+
+service postfix start
 service httpd start
 
 tail -f /var/log/httpd/*_log
