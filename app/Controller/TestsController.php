5fd093a2b3f7532d284f89e7432cf26b0f04ffe6
rog-works <rog_works@yahoo.co.jp>
2018-06-16 18:25:28 +0900

    feat: impl send mail

--- app/Controller/TestsController.php
+++ app/Controller/TestsController.php
@@ -1,9 +1,14 @@
 <?php
 App::uses('AppController', 'Controller');
+App::uses('CakeEmail', 'Network/Email');
 class TestsController extends AppController {
 
 	public function index() {
 		$this->Session->write('hoge', '1234');
+		$email = new CakeEmail();
+		$email->to('user@gmail.com')
+			->subject('from cakephp')
+			->send('cake email test');
 	}
 
 	public function complete() {
